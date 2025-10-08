47960251e59b9eecb03a3e769378f8c5d17ede0c
Ankit Sethi <ankitsethi@gmail.com>
2025-10-08 02:45:34 +0200

    small p

--- infra/terraform_resources/lambda.tf
+++ infra/terraform_resources/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = var.lambda_name
   role          = data.aws_iam_role.lambda_iam_role.arn
   handler       = "lambda_function.lambda_handler"
-  runtime       = "Python 3.13"
+  runtime       = "python 3.13"
   filename      = "${path.module}/lambda/lambda.zip"
   timeout       = 10
   environment {
